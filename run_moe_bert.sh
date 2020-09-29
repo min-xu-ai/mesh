cf85f65abd63b5c7691d123d7f617c494ffd92c9
Min Xu <m1n@fb.com>
2020-09-28 17:59:43 -0700

    add run script

---
+++ run_moe_bert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+PYTHONPATH=. python3 mesh_tensorflow/bert/run_pretraining.py   \
+     	--bert_config_file=mesh_tensorflow/bert/config/moe.json    \
+	--input_train_files="/private/home/m1n/local/bert_data_processed/pretraining_data/*"    \
+	--input_eval_files="/private/home/m1n/local/bert_data_processed/eval/*"  \
+	--output_dir=/tmp/moe_bert
